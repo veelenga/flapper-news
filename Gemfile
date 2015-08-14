39eff07a0a504596e4ad9a1083822049a0884232
veelenga <velenhaupt@gmail.com>
2015-08-14 16:40:41 +0300

    Wire frontend and backend

--- Gemfile
+++ Gemfile
@@ -5,8 +5,10 @@ gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
+gem 'responders', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'angular-rails-templates'
+gem 'angular_rails_csrf'
 
 group :development, :test do
   gem 'byebug'
--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,18 +1,23 @@
 angular.module('flapperNews')
   .controller('PostsCtrl', [
-    '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
-      $scope.post = posts.posts[$stateParams.id];
+    '$scope',
+    'posts',
+    'post',
+    function($scope, posts, post) {
+      $scope.post = post
 
       $scope.incrementUpvotes = function(comment){
-        comment.upvotes += 1;
+        posts.upvoteComment(post, comment);
       }
 
       $scope.addComment = function() {
         if ($scope.body === '') { return; }
-        $scope.post.comments.push({
+        posts.addComment(post.id, {
           body: $scope.body,
           author: 'user',
           upvotes: 0
+        }).success(function(comment) {
+          $scope.post.comments.push(comment);
         });
         $scope.body = '';
       }
